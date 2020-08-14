22ef206949e215aac504dd28913456ae758e7d54
Kaifee Haque <kaifeeahaque@outlook.com>
2020-08-14 16:07:55 -0400

    Add file storage to the multi clipboard.

--- CH06/multiclip.py
+++ CH06/multiclip.py
@@ -1,7 +1,10 @@
 #! python3
-import sys, pyperclip
+import sys, pyperclip, shelve
+from pathlib import Path
 
-entries = {"reaches": """--it reaches out it reaches out it reaches out it reaches out--
+shelf_file = shelve.open("clip")
+if len(shelf_file) == 0:
+    entries = {"reaches": """--it reaches out it reaches out it reaches out it reaches out--
 One hundred and thirteen times a second, nothing answers and it reaches out.
 It is not conscious, though parts of it are.
 There are structures within it that were once separate organisms; aboriginal, evolved, and complex.
@@ -11,6 +14,10 @@ The conscious parts try to make sense of the reaching out. Try to interpret it."
            "book": """It was a real book— onionskin pages bound in what might have been
 actual leather. Miller had seen pictures of them before; the idea of that
 much weight for a single megabyte of data struck him as decadent."""}
+    shelf_file["entries"] = entries
+    shelf_file.close()
+else:
+    entries = shelf_file[entries]
 
 if __name__ != "__main__":
     print("Program must be executed from the command line.")
@@ -18,9 +25,10 @@ if __name__ != "__main__":
 
 if len(sys.argv) < 3 or len(sys.argv) > 4:
     print("""Usage:
-To copy an entry: python multi_clipboard copy <keyphrase>
-To add an entry: python multi_clipboard add <keyphrase> <text>
-To remove an entry: python multi_clipboard delete <keyphrase>""")
+To copy an entry: python multiclip copy <keyphrase>
+To add an entry: python multiclip add <keyphrase> <text>
+To remove an entry: python multiclip delete <keyphrase>
+To clear the clipboard: python multiclip delete <keyphrase>""")
 
 command = sys.argv[1]
 keyphrase = sys.argv[2]
@@ -35,6 +43,12 @@ if command == "copy":
 
 elif command == "add" and sys.argv == 4:
     entries[keyphrase] == text
+    shelf_file = shelf_file = shelve.open("clip")
+    shelf_file["entries"] = entries
+    shelf_file.close()
 
 elif command == "delete":
     del entries[keyphrase]
+    shelf_file = shelf_file = shelve.open("clip")
+    shelf_file["entries"] = entries
+    shelf_file.close()
