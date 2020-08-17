beaa394c329d9c8baec342a16781f4af445f0e1e
Kaifee Haque <kaifeeahaque@outlook.com>
2020-08-17 15:39:33 -0400

    Added docstring and comments to the date detection project.

--- CH07/date_detection.py
+++ CH07/date_detection.py
@@ -1,18 +1,36 @@
 #! python3
 import re
 
-valid_date = re.compile(r"([0-3]\d)(/|-|.)([0-1]\d)(/|-|.)([1-2]\d\d\d)")
+def find_valid_dates(text):
+    """Searches for valid dates in a given string.
 
-def find_valid_date(text):
+    Args:
+        text: The string to be searched.
+
+    Returns:
+        dates: A list of tuples containing the components of any valid date
+        found within the string.
+    """
+    
+    valid_date = re.compile(r"([0-3]\d)(/|-|.)([0-1]\d)(/|-|.)([1-2]\d\d\d)")
     dates = valid_date.findall(text)
+    
     for date in dates:
+
+        #Removes the date from the list if:
+        #-the day is greater than 31
+        #-the month is greater than 12
+        #-the given day should not occur in the given month
+        
         if (date[0][0] == "3" and date[0][1] > 1)\
         or (date[2][0] == 1 and date[2][1] > 2)\
-        or (date[2] in ["04, 06, 09, 11"] and date[0] != 30)\
-        or (date[2] in ["01","03","05","07","08","10","12"] and date[0] != 31):
+        or (date[2] in ["04, 06, 09, 11"] and date[0] > 30)\
+        or (date[2] in ["01","03","05","07","08","10","12"] and date[0] == 31):
             del date
-        elif (date[2] == "02" and date[0] != 28)\
+        elif (date[2] == "02" and date[0] > 28)\
         or (date[4] % 4 == 0 and date[4] % 100 != 0 )\
-        and (date[2] == "02" and date[0] != 29):
+        and (date[2] == "02" and date[0] >= 29):
             del date
     return dates
+
+print(find_valid_dates("30/02/2020"))
