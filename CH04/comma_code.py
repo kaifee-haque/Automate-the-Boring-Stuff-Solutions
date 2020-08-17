765fbfc81b30b257af6940e6e92fd3ca215094e0
Kaifee Haque <kaifeeahaque@outlook.com>
2020-08-17 14:51:18 -0400

    Add docstrings and shebang line to the comma code projects.

--- CH04/comma_code.py
+++ CH04/comma_code.py
@@ -1,4 +1,15 @@
+#! python3
+
 def comma_string(_list):
+    """Takes a list of items and formats it into a string, separated by
+    commas like plain English.
+
+    Args:
+        _list: The list of items.
+
+    Returns:
+        result: The string of list items separated by commas."""
+    
     result = ""
     for i, character in enumerate(_list):
         if i == len(_list) - 1:
